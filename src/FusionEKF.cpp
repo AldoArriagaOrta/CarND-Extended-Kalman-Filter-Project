84e1f5c311a67ba1933d3a47b10ab1575c27923c
Aldo Arriaga <aldo.orta@eu4m.eu>
2018-02-09 22:52:25 +0100

    Extended Kalman Filter code

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -23,21 +23,19 @@ FusionEKF::FusionEKF() {
   Hj_ = MatrixXd(3, 4);
 
   //measurement covariance matrix - laser
-  R_laser_ << 0.0225, 0,
-        0, 0.0225;
+  R_laser_ <<	0.0225, 0,
+				0, 0.0225;
 
   //measurement covariance matrix - radar
-  R_radar_ << 0.09, 0, 0,
-        0, 0.0009, 0,
-        0, 0, 0.09;
+  R_radar_ <<	0.09, 0, 0,
+				0, 0.0009, 0,
+				0, 0, 0.09;
 
-  /**
-  TODO:
-    * Finish initializing the FusionEKF.
-    * Set the process and measurement noises
-  */
+  H_laser_ <<	1, 0, 0, 0,
+				0, 1, 0, 0;
 
-
+  noise_ax = 9;
+  noise_ay = 9;
 }
 
 /**
@@ -47,34 +45,61 @@ FusionEKF::~FusionEKF() {}
 
 void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
-
   /*****************************************************************************
    *  Initialization
    ****************************************************************************/
   if (!is_initialized_) {
-    /**
-    TODO:
-      * Initialize the state ekf_.x_ with the first measurement.
-      * Create the covariance matrix.
-      * Remember: you'll need to convert radar from polar to cartesian coordinates.
-    */
-    // first measurement
+
+    // Initialization of state vector ekf_.x_ with first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    ekf_.x_ << 1, 1, 0, 0;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      /**
-      Convert radar from polar to cartesian coordinates and initialize state.
-      */
+
+		//Initialization for radar measurement
+		Hj_ = tools.CalculateJacobian(ekf_.x_);
+		ekf_.R_ = MatrixXd(3, 3);
+		ekf_.R_ << R_radar_;
+		ekf_.H_ = MatrixXd(3, 4);
+		ekf_.H_ << Hj_;
+
+		//intermediate variables (to avoid unnecessary calls to trigonometric functions)
+		double Cos_theta = cos(measurement_pack.raw_measurements_[1]);
+		double Sin_theta = sin(measurement_pack.raw_measurements_[1]);
+
+		//mapping polar radar measurements to Cartesian state variables
+		ekf_.x_ <<	measurement_pack.raw_measurements_[0] * Cos_theta,
+					measurement_pack.raw_measurements_[0] * Sin_theta,
+					measurement_pack.raw_measurements_[2] * Cos_theta,
+					measurement_pack.raw_measurements_[2] * Sin_theta;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-      Initialize state.
-      */
+
+      //Initialization for lidar measurement
+	  //set the state with the initial location and zero velocity
+		ekf_.H_ = MatrixXd(2, 4);
+		ekf_.H_ << H_laser_;
+		ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
     }
 
+	//state covariance matrix P
+	ekf_.P_ = MatrixXd(4, 4);
+	ekf_.P_ <<	1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 10, 0,
+				0, 0, 0, 10;
+
+	//the initial transition matrix F_
+	ekf_.F_ = MatrixXd(4, 4);
+	ekf_.F_ <<	1, 0, 1, 0,
+				0, 1, 0, 1,
+				0, 0, 1, 0,
+				0, 0, 0, 1;
+
     // done initializing, no need to predict or update
+	// but first timestamp is needed for the next cycle
+	previous_timestamp_ = measurement_pack.timestamp_;
     is_initialized_ = true;
     return;
   }
@@ -83,13 +108,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Prediction
    ****************************************************************************/
 
-  /**
-   TODO:
-     * Update the state transition matrix F according to the new elapsed time.
-      - Time is measured in seconds.
-     * Update the process noise covariance matrix.
-     * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
+   //compute the time elapsed between the current and previous measurements
+  double dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+
+  //1. Modify the F matrix so that the time is integrated
+  ekf_.F_ <<	1, 0, dt, 0,
+				0, 1, 0, dt,
+				0, 0, 1, 0,
+				0, 0, 0, 1;
+  //2. Set the process covariance matrix Q
+  double t4 = (dt*dt*dt*dt) / 4;
+  double t3 = (dt*dt*dt) / 2;
+  double t2 = dt*dt;
+
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ <<	t4*noise_ax, 0, t3*noise_ax, 0,
+				0, t4*noise_ay, 0, t3*noise_ay,
+				t3*noise_ax, 0, t2*noise_ax, 0,
+				0, t3*noise_ay, 0, t2*noise_ay;
 
   ekf_.Predict();
 
@@ -97,16 +135,21 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Update
    ****************************************************************************/
 
-  /**
-   TODO:
-     * Use the sensor type to perform the update step.
-     * Update the state and covariance matrices.
-   */
-
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+	  Hj_ = tools.CalculateJacobian(ekf_.x_);
+	  ekf_.R_ =  MatrixXd(3, 3);
+	  ekf_.R_ << R_radar_;
+	  ekf_.H_ = MatrixXd(3, 4);
+	  ekf_.H_ << Hj_;
+	  ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+	  ekf_.R_ = MatrixXd(2, 2);
+	  ekf_.R_ << R_laser_;
+	  ekf_.H_ = MatrixXd(2, 4);
+	  ekf_.H_ << H_laser_;
+	  ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,73 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+	
+	//this method was taken directly from the lesson
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if (estimations.size() != ground_truth.size()
+		|| estimations.size() == 0) {
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+	//accumulate squared residuals
+	for (unsigned int i = 0; i < estimations.size(); ++i) {
+
+		VectorXd residual = estimations[i] - ground_truth[i];
+
+		//coefficient-wise multiplication
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	//calculate the mean
+	rmse = rmse *(1.0/ estimations.size());
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+
+	MatrixXd Hj(3, 4);
+
+	//fetch state variables
+	double px = x_state(0);
+	double py = x_state(1);
+	double vx = x_state(2);
+	double vy = x_state(3);
+
+	//intermediate computations (to avoid duplications)
+	double psq = (px*px) + (py*py);
+	double pmag = sqrt(psq);
+	double p32 = pmag*psq;
+
+	//check division by zero. x^2 + y^2 > 0
+	if (psq < 0) {
+		cout << "Division by zero error" << endl;
+		return Hj;
+	}
+
+	//matrix elements to avoid duplicating computations
+	double h11 = px / pmag;
+	double h12 = py / pmag;
+	double h21 = -py / psq;
+	double h22 = px / psq;
+	double h31 = (py*(vx*py - vy*px)) / p32;
+	double h32 = (px*(vy*px - vx*py)) / p32;
+
+	//Jacobian Matrix. Some elements are repeated to avoid unnecessary variables
+	Hj <<	h11, h12, 0, 0,
+			h21, h22, 0, 0,
+			h31, h32, h11, h12;
+
+	return Hj;
 }
